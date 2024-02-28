cc838289e0aefc0ba90901150ef8234c07904a07
jakpia0279 <jakpia0279@W04N-P317D11>
2024-02-28 19:46:46 +0100

    Add project files.

---
+++ Lab0_NET/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0_NET
+{
+    internal class FizzBuzz
+    {
+        private int upperBound;
+
+        public FizzBuzz(int uB)
+        {
+            upperBound = uB;
+        }
+
+        public void Display()
+        {
+            for(int i = 1; i < upperBound; i++)
+            {
+                string fizzbuzz = "";
+
+                if (i % 3 == 0)
+                {
+                    fizzbuzz += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    fizzbuzz += "Buzz";
+                }
+
+                if(fizzbuzz == "")
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(fizzbuzz);
+                }
+            }
+        }
+    }
+}
---
+++ Lab0_NET/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab0_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new FizzBuzz(16);
+            fz1.Display();
+        }
+    }
+}
